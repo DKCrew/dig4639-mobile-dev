312018363f1a3ca00fbe926586460cbe52080ac6
Jeremy Williams <je454265@ucf.edu>
2020-01-15 10:06:01 -0500

    ...

--- ice/w2a/node_index.js
+++ ice/w2a/node_index.js
@@ -1,11 +1,20 @@
-function scopeDemo() {
+console.log("Hello WOrld");
+//var x =1;
+var y =[1];
+console.log("Test");
+function scopeDemo(y1) {
     console.log(x)
     let a = 3, b = 4, c = 5;
     console.log(a);
     console.log(b);
     console.log(c);
+    console.log(y1);
+    y1[0] =500;
     var x = 2;
     if(true) {
-        var x = 0;
+        var x = 2;
     }
+    return a;
 }
+console.log(scopeDemo(y)+10);
+console.log(y);
