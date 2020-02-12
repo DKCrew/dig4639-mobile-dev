95d1443510e9b161f323f623c894d09c8421cbe0
DKCrew <59653608+DKCrew@users.noreply.github.com>
2020-02-12 10:05:45 -0500

    Updates

---
+++ precache-manifest.bae8f0037c52678631102c3141500740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e56cf24042f15cf911b3148fb3ee4b",
+    "url": "/DKCrew/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "37d714fe585a673094b4",
+    "url": "/DKCrew/dig4639-mobile-dev/static/css/main.2a34271c.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/DKCrew/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/DKCrew/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d714fe585a673094b4",
+    "url": "/DKCrew/dig4639-mobile-dev/static/js/main.5b3be756.chunk.js"
+  },
+  {
+    "revision": "afab120db548fa95749b",
+    "url": "/DKCrew/dig4639-mobile-dev/static/js/runtime-main.b6639c96.js"
+  }
+]);
