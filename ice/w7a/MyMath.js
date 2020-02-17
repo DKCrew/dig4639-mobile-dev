a057eed417026270a3b0005c41605eef13ee7602
DKCrew <59653608+DKCrew@users.noreply.github.com>
2020-02-17 10:06:01 -0500

    Commit

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,3 +1,24 @@
+/**
+ * two parameters must be numbers, return undefined if not
+ * @param a The first number
+ * @param b The second number
+ */
 export function Sum (a, b) {
-  return a + b
+  if(typeof a == 'number' && typeof b =='number')
+    return (a+b);
+  else
+    return undefined;
+}
+
+export function Addlist(list) {
+  if(typeof list == 'number')
+  var list = [1,2,3,4]
+  return list;
+}
+
+export function DivideBy (a, b) {
+  if(typeof a =='number' && typeof b =='number' && b !== 0)
+    return (a/b);
+  else 
+    return undefined;
 }
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,10 +1,35 @@
-import { Sum } from './sum.js'
+import { Sum, Addlist, DivideBy } from './MyMath.js'
 
+describe('Testing Sum', () => {
+it('Adds a string and a number', () => {
+  var result = Sum("Test", 5);
+  expect(result).toBeUndefined()
+});
 test('adds 1 + 2 to equal 3', () => {
   expect(Sum(1, 2)).toBe(3)
 })
+test('border cases', () => {
+  expect(Sum(-10, 5)).toBe(-5)
+  //expect(Sum(.1, .2)).toBe(.3)
+  expect(Sum(10000000000000000000, 2)).toBe(10000000000000000002)
+})
 
-// ICE 
+describe('Testing Addlist', () =>
+{
+  let list = [1,2,3,4];
+  let result = Addlist(list);
+  expect(result).toBe(10);
+})
+
+describe('testing Divideby', () =>{
+
+})
+
+});
+
+
+
+
 
 // Examples for Null
 test('null', () => {
