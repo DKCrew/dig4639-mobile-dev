49da7ba6322008730be5fdefa70da7e4aa50ba08
DKCrew <59653608+DKCrew@users.noreply.github.com>
2020-02-26 10:12:06 -0500

    commit 2/26 class

--- demo/w8a/todo-react/src/App.js
+++ demo/w8a/todo-react/src/App.js
@@ -1,25 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import todoList from './todoList.json' 
 
-const todoList1 = [
-  { completed: true, priority: 1, content: 'Complete demo' },
-  { completed: true, priority: 2, content: 'Complete demo 2' },
-  { completed: false, priority: 1, content: 'Complete demo' }
-]
-
-function TodoItem(props) {
-  return <p>{props.cotent}</p>
+class TodoItem extends React.Component{
+  render(props){
+  return <p className='card'>{props.content}</p>
+  }
 }
 
-function App(){
-  let array = todolist1.map((value) => 
-<TodoItem content={value.content}
-priority = {value.priority}  />
-  )
-  return(
-    array
-      );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state ={
+    todoList: todoList,
+    showOnlyUncompletedTasks: false
+  }
+  this.currentID = 4
 }
 
+removeTask(){
+
+let newList = this.state.todoList.filter((task)=>(task.id != taskID))
+this.setState({todoList:newList})
+}
+
+addTask(){
+  console.log(this.refs.taskContent.value)
+ let newTask =
+ { "id":this.state.currentID,
+ "content": "Task 3", "priority": 3, "completed": false }
+ this.currentID++;
+ let currentList = this.state.todoList
+ currentList.push(newTask)
+ this.setState({todoList:currentList})
+}
+
+render() {
+  return (
+  <>
+  <div className= "inputTask">
+    <input type ="text" ref="taskContent"/>
+    <select ref = "priority" name="priority">
+      <option value = "1">1</option>
+      <option value = "2">2</option>
+      <option value = "3">3</option>
+    </select>
+    <input type="button" value="Add Task" onClick={() => this.addTask()}/>
+    <input type="checkbox" ref="showOnlyUncompletedTasks" 
+      onChange={(e) => this.setState({showOnlyUncompletedTasks: e.target.checked})}
+      id="showOnlyUncompletedTasks"
+      name ="showOnlyUncompletedTasks" value = "completed" />
+    
+    <label htmlFor="showOnlyUncompletedTasks"> Show only uncompleted tasks</label>
+    </div>
+    {(this.state.showOnlyUncompletedTasks ? 
+    this.state.todoList.filter((v) => v.completed) : this.state.todoList)
+    .map((v, i) => <TodoItem key ={i} priority={v.priority} 
+      removeTask={(id) => this.removeTask(id)}
+      content={v.content} completed={v.completed}/>)
+    }
+    
+  </>
+  );
+}
+}
 export default App;
