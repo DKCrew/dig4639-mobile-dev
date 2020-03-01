e5d70c766f161f097ba17e8ca2f13684d1850044
DKCrew <jwilliams95@knights.ucf.edu>
2020-03-01 15:30:00 -0500

    Final lab 4 Changes

--- labs/4/src/App.js
+++ labs/4/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import CardList from './components/CardList';
 import'./App.css';
 
-function App(){
+class App extends React.Component{
   //wrapped Cardlist in a div for easier style editing if needed
+  render(){
     return(
       <div class= "cardApp">
       <CardList/>
       </div>
-  )
+    );
   }
+}
 
 
 export default App;
