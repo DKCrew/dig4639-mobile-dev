4f631c976d04b8854356eb94f2c96b187f5d0e43
DKCrew <jwilliams95@knights.ucf.edu>
2020-03-01 19:06:40 -0500

    Lab4 Final

--- labs/4/src/components/CardList/index.js
+++ labs/4/src/components/CardList/index.js
@@ -3,18 +3,25 @@ import data from '../../data.json'
 import Card from '../Card/index.js'
 
 class CardList extends React.Component {
-constructor (props){
-    super(props)
+
+    constructor (props){
+        super(props)
 
     this.state = {
+        
         cards: data.cards
+        
         }
     }
 //function for deleting a card, used in Card/index.js
 deleteCard(title){
+    
     console.log("Card deleted!")
+    
+    
     console.log(this.state.cards)
 //sets new card state once cards have been filtered
+    
     let cards = this.state.cards.filter((v) => v.title !== title)
 
     this.setState({cards})
